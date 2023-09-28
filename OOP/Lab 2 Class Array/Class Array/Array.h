05b10b0d8f7598374b1a78d33a39c2c94fb79b47
Aleksandr2759 <sashagolovin2004nk@gmail.com>
2023-09-28 11:41:06 +0700

    Оператор копирования и неисправленные ошибки

--- OOP/Lab 2 Class Array/Class Array/Array.h
+++ OOP/Lab 2 Class Array/Class Array/Array.h
@@ -6,11 +6,14 @@ public:
 	Array(const int size = 10, const int value = 0);
 	~Array();
 
-	void print();
 
-	int& operator[] (const int index);
+	int size() const;
+	void print() const;
+
+	const int& operator[] (const int index);
+
 
 private:
 	int* m_array = nullptr;
 	int m_size = 0;
-}
+};
